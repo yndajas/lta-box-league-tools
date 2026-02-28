8034f8113cf8663bbb560db0ffbb93ae6ad03709
Ynda Jas <yndajas@gmail.com>
2026-02-28 00:15:54 +0000

    Remove unused table style

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ async function populatedGroups() {
 
 async function seasonTable() {
   return `
-<table border="1" cellpadding="0" cellspacing="0" style="width:fit-content; max-width:100%">
+<table border="1" cellpadding="0" cellspacing="0" style="width:fit-content;">
 	<tbody>
 		<tr>
 			<td><strong>Box</strong></td>
